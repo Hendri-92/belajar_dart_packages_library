aae8dd150f713230de6a2a095f6e1f5352bc88f4
Supuman Bunne <supumanbunne@gmail.com>
2026-01-18 14:44:02 +0800

    dart library

---
+++ example/tes_packages.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_packages_library/hello.dart';
+
+void main() {
+
+  print(sayHello('Iwan'));
+
+  // var customer = Customer();
+  // var category = Category();
+  // var product = Product();
+}
